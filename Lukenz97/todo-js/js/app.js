b0d29302dea6726878c8f9c9d12d6dd6e5d261e1
Lukenz97 <57329705+Lukenz97@users.noreply.github.com>
2019-12-02 02:31:09 +0100

    Update-structure

--- Lukenz97/todo-js/js/app.js
+++ Lukenz97/todo-js/js/app.js
@@ -1,142 +1,146 @@
-window.onload = function(){
-    let b = window.localStorage.getItem("todo");
-    if(b === undefined || b === null){
-        window.localStorage.setItem("todo", JSON.stringify(new Array)); // prazan niz
-        b = JSON.stringify(new Array);
+window.onload = function() {
+  let toDoList = window.localStorage.getItem("todo");
+  if (!toDoList) {
+    window.localStorage.setItem("todo", JSON.stringify(new Array())); // prazan niz
+    toDoList = JSON.stringify(new Array());
+  }
+  fillList(JSON.parse(toDoList));
+};
+
+function myFunction(event) {
+  event.preventDefault();
+  const inputValue = document.getElementById("input-todo").value; // pravimo konstantu koja uzima vrednosti onog sto smo ukucali
+  let toDoList = JSON.parse(window.localStorage.getItem("todo")); // pravimo promenljivu koja ucitava vrednosti iz local-storage-a kao objekat(niz)
+  toDoList.push([inputValue, false]); //a stavljamo u b
+  window.localStorage.setItem("todo", JSON.stringify(toDoList)); // stavljamo u local storage taj niz sa novim elemtnom koji smo uneli
+  fillList(toDoList); // popunili listu sa itemom koji smo uneli
+  document.getElementById("input-todo").value = ""; // cistimo input
+}
+
+function fillList(list) {
+  if (!list) return;
+  let toDoList = document.getElementById("todo-list");
+  while (toDoList.hasChildNodes()) {
+    toDoList.removeChild(toDoList.firstChild); // brisemo prethodne vrednosti iz niza
+  }
+  for (let i = 0; i < list.length; i++) {
+    document
+      .getElementById("todo-list")
+      .appendChild(createListItem(list[i][0], list[i][1], i));
+  }
+}
+
+function deleteItem(id) {
+  const splitId = id.split("_");
+  const index = parseInt(splitId[1]);
+  let toDoList = JSON.parse(window.localStorage.getItem("todo"));
+  toDoList.splice(i, 1);
+  window.localStorage.setItem("todo", JSON.stringify(b));
+  fillList(toDoList);
+}
+
+function showAll() {
+  let toDoList = JSON.parse(window.localStorage.getItem("todo"));
+  fillList(toDoList);
+}
+
+function showActive(showAct) {
+  let toDoList = JSON.parse(window.localStorage.getItem("todo"));
+  let tempArray = new Array();
+  for (let i = 0; i < toDoList.length; i++) {
+    if (toDoList[i][1] === !showAct) {
+      tempArray.push(toDoList[i]);
     }
-    fillList(JSON.parse(b));
+  }
+  fillList(tempArray);
 }
-function myFunction(event){
-    event.preventDefault();
-    const a = document.getElementById("input-todo").value; // pravimo konstantu koja uzima vrednosti onog sto smo ukucali 
-    let b = JSON.parse(window.localStorage.getItem("todo"));// pravimo promenljivu koja ucitava vrednosti iz local-storage-a kao objekat(niz)
-    b.push([a, false]); //a stavljamo u b
-    window.localStorage.setItem("todo", JSON.stringify(b)); // stavljamo u local storage taj niz sa novim elemtnom koji smo uneli
-    fillList(b);// popunili listu sa itemom koji smo uneli
-    document.getElementById("input-todo").value = '';// cistimo input 
+
+function checkedItem(id) {
+  // id = "item_x"
+  const splitId = id.split("_");
+  const index = parseInt(splitId[1]);
+  let toDoList = JSON.parse(window.localStorage.getItem("todo"));
+  toDoList[index][1] = !toDoList[index][1];
+  window.localStorage.setItem("todo", JSON.stringify(toDoList));
+  document.getElementById(id).style.background =
+    toDoList[index][1] === true ? "navajowhite" : "white";
 }
-function fillList(list){
-    if(list === undefined || list === null || list === "")
-        return;
-    let lista = document.getElementById("lista")
-    while (lista.hasChildNodes()) {  
-        lista.removeChild(lista.firstChild); // brisemo prethodne vrednosti iz niza
-      }
-    for(let i = 0; i < list.length; i++) {
-        document.getElementById("lista").appendChild(createListItem(list[i][0], list[i][1], i));
+
+function clearAll() {
+  window.localStorage.setItem("todo", JSON.stringify(new Array()));
+  let toDoList = document.getElementById("todo-list");
+  while (toDoList.hasChildNodes()) {
+    toDoList.removeChild(toDoList.firstChild); // brisemo prethodne vrednosti iz niza
+  }
+}
+
+function clearCompleted() {
+  let toDoList = JSON.parse(window.localStorage.getItem("todo"));
+  for (let i = 0; i < toDoList.length; i++) {
+    if (toDoList[i][1] === true) {
+      toDoList.splice(i, 1);
+      i--;
     }
+  }
+  window.localStorage.setItem("todo", JSON.stringify(toDoList));
+  fillList(toDoList);
 }
-function deleteItem(id){
-    let s = id.split("_");
-    let i = parseInt(s[1]);
-    let b = JSON.parse(window.localStorage.getItem("todo"));
-    b.splice(i,1)
-    window.localStorage.setItem("todo", JSON.stringify(b));
-    fillList(b);
 
+function createListItem(text, isChecked, index) {
+  let itemEle1 = document.createElement("div");
+  itemEle1.setAttribute("class", "input-group border");
+  itemEle1.id = "item_" + index;
+  itemEle1.style.background = isChecked === true ? "navajowhite" : "white";
 
+  let itemEle2 = document.createElement("div");
+  let textnode1 = document.createTextNode(text);
+  itemEle2.appendChild(textnode1);
 
-}
-function showAll(){
-    let b = JSON.parse(window.localStorage.getItem("todo"));
-    fillList(b);
-}
-function showActive(x){
-    let b = JSON.parse(window.localStorage.getItem("todo"));
-    let c = new Array;
-    for(let i = 0; i < b.length; i++) {
-        if(b[i][1] === !x){
-            c.push(b[i]);
-        }
-    }
-    fillList(c);
-}
-function checkedItem(id){ // id = "item_x"
-    let s = id.split("_");
-    let i = parseInt(s[1]);
-    let b = JSON.parse(window.localStorage.getItem("todo"));
-    b[i][1] = !b[i][1]
-    window.localStorage.setItem("todo", JSON.stringify(b));
-    if(b[i][1] === true){
-        document.getElementById(id).style.background = 'navajowhite'
-    }else {
-        document.getElementById(id).style.background = 'white'
-    }
-}
-function clearAll(){
-    window.localStorage.setItem("todo", JSON.stringify(new Array));
-    let lista = document.getElementById("lista")
-    while (lista.hasChildNodes()) {  
-        lista.removeChild(lista.firstChild); // brisemo prethodne vrednosti iz niza
-      }
-}
-function clearCompleted(){
-    let b = JSON.parse(window.localStorage.getItem("todo"));
-    for(let i = 0; i < b.length; i++){
-        if(b[i][1] === true){
-            b.splice(i,1);
-            i--;
-        }
-    }
-    window.localStorage.setItem("todo", JSON.stringify(b));
-    fillList(b);
-}
-function createListItem(text, isChecked, index){
-    var div_1 = document.createElement("div");
-    div_1.setAttribute('class',"input-group border");
-    div_1.id = "item_" + index
-    if(isChecked === true){
-        div_1.style.background = 'navajowhite'
-    }else {
-        div_1.style.background = 'white'
-    }
-    
-    var div_2 = document.createElement("div");
-    var textnode1 = document.createTextNode(text);
-    div_2.appendChild(textnode1);
+  itemEle1.appendChild(itemEle2);
 
-    div_1.appendChild(div_2);
+  let itemEle3 = document.createElement("div");
+  itemEle3.setAttribute("class", "input-group-append right");
 
-    var div_3 = document.createElement("div");
-    div_3.setAttribute('class',"input-group-append right");
+  let itemEle4 = document.createElement("button");
+  itemEle4.setAttribute("class", "btn btn-outline-secondary");
+  itemEle4.setAttribute("type", "button");
+  let textnode2 = document.createTextNode("check");
+  itemEle4.appendChild(textnode2);
+  itemEle4.setAttribute("onclick", 'checkedItem("item_' + index + '")');
 
-    var div_4 = document.createElement("button");
-    div_4.setAttribute('class',"btn btn-outline-secondary");
-    div_4.setAttribute('type',"button");
-    var textnode2 = document.createTextNode("check");
-    div_4.appendChild(textnode2);
-    div_4.setAttribute('onclick', 'checkedItem("item_' + index + '")')
+  let itemEle5 = document.createElement("button");
+  itemEle5.setAttribute(
+    "class",
+    "btn btn-outline-secondary dropdown-toggle dropdown-toggle-split"
+  );
+  itemEle5.setAttribute("type", "button");
+  itemEle5.setAttribute("data-toggle", "dropdown");
+  itemEle5.setAttribute("aria-haspopup", "true");
+  itemEle5.setAttribute("aria-expanded", "false");
 
-    var div_5 = document.createElement("button");
-    div_5.setAttribute('class',"btn btn-outline-secondary dropdown-toggle dropdown-toggle-split");
-    div_5.setAttribute('type',"button");
-    div_5.setAttribute('data-toggle',"dropdown");
-    div_5.setAttribute('aria-haspopup',"true");
-    div_5.setAttribute('aria-expanded',"false");
-    
-    div_3.appendChild(div_4);
-    div_3.appendChild(div_5);
+  itemEle3.appendChild(itemEle4);
+  itemEle3.appendChild(itemEle5);
 
-    var div_6 = document.createElement("div");
-    div_6.setAttribute('class',"dropdown-menu");
+  let itemEle6 = document.createElement("div");
+  itemEle6.setAttribute("class", "dropdown-menu");
 
-    var a_1 = document.createElement("a");
-    a_1.setAttribute('class',"dropdown-item");
-    var textnode3 = document.createTextNode("check");
-    a_1.appendChild(textnode3);
-    a_1.setAttribute('onclick', 'checkedItem("item_' + index + '")')
+  let itemEle7 = document.createElement("a");
+  itemEle7.setAttribute("class", "dropdown-item");
+  let textnode3 = document.createTextNode("check");
+  itemEle7.appendChild(textnode3);
+  itemEle7.setAttribute("onclick", 'checkedItem("item_' + index + '")');
 
-    var a_2 = document.createElement("a");
-    a_2.setAttribute('class',"dropdown-item");
-    var textnode4 = document.createTextNode("delete");
-    a_2.setAttribute('onclick', 'deleteItem("item_' + index + '")')
-    a_2.appendChild(textnode4);
+  let itemEle8 = document.createElement("a");
+  itemEle8.setAttribute("class", "dropdown-item");
+  let textnode4 = document.createTextNode("delete");
+  itemEle8.setAttribute("onclick", 'deleteItem("item_' + index + '")');
+  itemEle8.appendChild(textnode4);
 
-    div_6.appendChild(a_1);
-    div_6.appendChild(a_2);
+  itemEle6.appendChild(itemEle7);
+  itemEle6.appendChild(itemEle8);
 
-    div_3.appendChild(div_6);
-    div_1.appendChild(div_3);
+  itemEle3.appendChild(itemEle6);
+  itemEle1.appendChild(itemEle3);
 
-    return div_1;
+  return itemEle1;
 }
